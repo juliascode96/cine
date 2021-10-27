aed2e7fbe650b9694c0c5211874811a55336915f
Julia Scodelari <j.scodelari96@gmail.com>
2021-10-26 21:03:42 -0300

    Guardado del proyecto

---
+++ src/salascine/SalasCine.java
@@ -0,0 +1,11 @@
+
+package salascine;
+
+public class SalasCine {
+
+    public static void main(String[] args) {
+        Cine maravilloso = new Cine();
+        maravilloso.nocheDePelis(); 
+    }
+    
+}
